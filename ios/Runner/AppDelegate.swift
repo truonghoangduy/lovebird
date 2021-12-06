b5b80dd66da65becfaaacae1cb8950032d8ec6dd
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-12-06 21:07:56 +0700

    remove blank

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,18 +10,6 @@ import CoreBluetooth
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       
-      let rootViewController : FlutterViewController = window?.rootViewController as! FlutterViewController
-
-      
-      let ble_mac_platfrom_channel_name = "flutter.native/ble_mac_address"
-      
-      let ble_mac_platfrom_channel = FlutterMethodChannel(name: ble_mac_platfrom_channel_name, binaryMessenger: rootViewController as! FlutterBinaryMessenger)
-      
-      ble_mac_platfrom_channel.setMethodCallHandler {(call: FlutterMethodCall, result: FlutterResult) -> Void in
-          if (call.method == "get_ble_mac") {
-              CBCentralManager.
-          }
-      }
       GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
