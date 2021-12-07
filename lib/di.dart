d1f80f1144338de971f88c00a075a27b073025f4
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-12-07 12:08:02 +0700

    bloc DI layer

---
+++ lib/di.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:lovebird/blocs/authentication/authentication_bloc.dart';
+import 'package:lovebird/services/auth/authenticaiton_repository.dart';
+import 'package:lovebird/services/auth/providers/authentication_firebase_provider.dart';
+import 'package:lovebird/services/auth/providers/google_sign_in_provider.dart';
+
+class DependecyProvider extends StatefulWidget {
+  final Widget child;
+  const DependecyProvider({Key? key, required this.child}) : super(key: key);
+
+  @override
+  _DependecyProviderState createState() => _DependecyProviderState();
+}
+
+class _DependecyProviderState extends State<DependecyProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      lazy: false,
+      create: (context) => AuthenticationRepository(
+        authenticationFirebaseProvider: AuthenticationFirebaseProvider(
+          firebaseAuth: FirebaseAuth.instance,
+        ),
+        googleSignInProvider: GoogleSignInProvider(
+          googleSignIn: GoogleSignIn(),
+        ),
+      ),
+      child: MultiBlocProvider(providers: [
+        BlocProvider(
+            lazy: false,
+            create: (context) {
+              return AuthenticationBloc(
+                  authenticationRepository:
+                      RepositoryProvider.of<AuthenticationRepository>(context));
+            })
+      ], child: widget.child),
+    );
+  }
+}
